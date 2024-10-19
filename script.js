54ee7c82ea8bd70e7e1f182c96cc3af0eaf4d0f3
amanda-bann <amanda_klap@protonmail.com>
2024-10-18 18:31:20 -0600

    Output strings with a for...of loop

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ var programmingLanguages = [
   "Rust",
   "C#"
 ];
+
+// Output Strings witha a for...of Loop //
+// Log out array
+console.log(programmingLanguages);
+
+// For..of loop
+for (var language of programmingLanguages) {
+  console.log(`I can't wait to learn ${language}!`);
+}
